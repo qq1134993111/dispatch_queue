6f485f9f49971232758cdcdb0685a462c3f301a5
qq1134993111 <1134993111@qq.com>
2021-03-09 21:52:53 +0800

    修改

--- MsgQ/dispatch_queue.cpp
+++ MsgQ/dispatch_queue.cpp
@@ -1,148 +1,137 @@
 #include "stdafx.h"
 #include "dispatch_queue.h"
 #include<algorithm>
+#include <iterator>
 #include <vector>
 DispatchQueue::DispatchQueue() :timer_thread_started_(false), work_queue_thread_started(false), quit_(false), generate_timer_id_(0), fall_through_(false)
 {
-	InitThread();
+    InitThread();
 }
 
 
 DispatchQueue::~DispatchQueue()
 {
-	Stop();
-	Join();
+    Stop();
+    Join();
 }
-
 
 
 uint64_t DispatchQueue::SetTimer(uint64_t milliseconds_timeout, EventFunc fun, bool repeat)
 {
-	if (!IsRunning())
-		return 0;
+    if (!IsRunning())
+        return 0;
 
-	std::unique_lock<decltype(timer_mtx_)> timer_lock(timer_mtx_);
-	Event_Entry event_entry(++generate_timer_id_, milliseconds_timeout, std::chrono::steady_clock::now() + std::chrono::milliseconds(milliseconds_timeout), repeat, std::move(fun), true);
-	if (!timers_set_.empty() && event_entry.next_run_ < timers_set_.begin()->next_run_)
-	{
-		fall_through_ = true;
-	}
-	timers_set_.insert(std::move(event_entry));
-	timer_cond_.notify_one();
+    std::unique_lock<decltype(timer_mtx_)> timer_lock(timer_mtx_);
+    Event_Entry event_entry(++generate_timer_id_, milliseconds_timeout, std::chrono::steady_clock::now() + std::chrono::milliseconds(milliseconds_timeout), repeat, std::move(fun), true);
+    if (!timers_set_.empty() && event_entry.next_run_ < timers_set_.begin()->next_run_)
+    {
+        fall_through_ = true;
+    }
+    timers_set_.insert(std::move(event_entry));
+    timer_cond_.notify_one();
 
-	return generate_timer_id_;
+    return generate_timer_id_;
 }
 
 bool DispatchQueue::CancelTimer(uint64_t timer_id)
 {
-	if (!IsRunning())
-		return false;
+    if (!IsRunning())
+        return false;
 
-	std::unique_lock<decltype(timer_mtx_)> timer_lock(timer_mtx_);
-	auto fun = [&](const Event_Entry& event_entry) {  return event_entry.id_ == timer_id; };
-	auto it = std::find_if(timers_set_.begin(), timers_set_.end(), fun);
-	if (it != timers_set_.end())
-	{
-		if (it == timers_set_.begin())
-		{
-			timers_set_.erase(it);
-			fall_through_ = true;
-			timer_cond_.notify_one();
+    std::unique_lock<decltype(timer_mtx_)> timer_lock(timer_mtx_);
+    auto fun = [&](const Event_Entry& event_entry) {  return event_entry.id_ == timer_id; };
+    auto it = std::find_if(timers_set_.begin(), timers_set_.end(), fun);
+    if (it != timers_set_.end())
+    {
+        if (it == timers_set_.begin())
+        {
+            timers_set_.erase(it);
+            fall_through_ = true;
+            timer_cond_.notify_one();
 
-			return true;
-		}
-		else
-		{
-			timers_set_.erase(it);
-			return true;
-		}
-	}
-	else
-	{
-		return false;
-	}
+            return true;
+        }
+        else
+        {
+            timers_set_.erase(it);
+            return true;
+        }
+    }
+    else
+    {
+        return false;
+    }
 
-	return true;
+    return true;
 }
 
 void DispatchQueue::DispatchThreadProc()
 {
-	//std::unique_lock< decltype(work_queue_mtx_) >  work_queue_lock(work_queue_mtx_);
-	//work_queue_cond_.notify_one();
-	work_queue_thread_started = true;
 
-	//decltype(work_queue_) dq;
-	std::vector<Event_Entry> vEvent(10240);
-	while (quit_ == false)
-	{
-		size_t len = work_concurrentqueue_.try_dequeue_bulk(vEvent.begin(), vEvent.size());
-		for (size_t i = 0; i < len; i++)
-		{
-			if (vEvent[i].event_handler_)
-			{
-				vEvent[i].event_handler_();
-			}
-		}
-	}
+    work_queue_thread_started = true;
+
+    std::vector<Event_Entry> vEvent(10240);
+    while (!quit_)
+    {
+        size_t len = work_concurrentqueue_.try_dequeue_bulk(vEvent.begin(), vEvent.size());
+        for (size_t i = 0; i < len; i++)
+        {
+            if (vEvent[i].event_handler_)
+            {
+                vEvent[i].event_handler_();
+            }
+        }
+    }
 
 }
 
 void DispatchQueue::TimerThreadProc()
 {
-	std::unique_lock< decltype(timer_mtx_) > timer_lock(timer_mtx_);
-	timer_cond_.notify_one();
-	timer_thread_started_ = true;
+    std::unique_lock< decltype(timer_mtx_) > timer_lock(timer_mtx_);
+    timer_thread_started_ = true;
+    timer_cond_.notify_one();
 
-	while (quit_ == false)
-	{
-		if (timers_set_.empty())
-		{
-			timer_cond_.wait(timer_lock, [&] { return quit_ || !timers_set_.empty(); });
-		}
 
-		while (!timers_set_.empty())
-		{
-			auto  work = *timers_set_.begin();
+    while (!quit_)
+    {
+        if (timers_set_.empty())
+        {
+            timer_cond_.wait(timer_lock, [&] { return quit_ || !timers_set_.empty(); });
+        }
 
-			if (timer_cond_.wait_until(timer_lock, work.next_run_, [this] { return quit_.load() || fall_through_.load(); }))
-			{
-				//等待条件完成
+        while (!timers_set_.empty())
+        {
+            auto  min_work = *timers_set_.begin();
 
-				fall_through_ = false;
+            if (timer_cond_.wait_until(timer_lock, min_work.next_run_, [this] { return quit_.load() || fall_through_.load(); }))
+            {
+                //等待条件完成
 
-				break;
-			}
-			else
-			{
-				//timeout
-				timers_set_.erase(timers_set_.begin());
-				timer_lock.unlock();
-				//work.event_handler_();
-				{
-					//插入队列
-					/*std::unique_lock< decltype(work_queue_mtx_) >  work_queue_lock(work_queue_mtx_);
+                fall_through_ = false;
 
-					auto where = std::find_if(work_queue_.rbegin(),
-						work_queue_.rend(),
-						[](Event_Entry const &e) { return !e.from_timer_; });
+                break;
+            }
+            else
+            {
+                //timeout
+                const auto end = timers_set_.upper_bound(min_work);
+                std::vector<Event_Entry> v_expired;
+                std::move(timers_set_.begin(), end, std::back_inserter(v_expired));
+                timers_set_.erase(timers_set_.begin(), end);
+                for (const auto& work : v_expired)
+                {
+                    if (work.repeat_)
+                    {
+                        timers_set_.insert(Event_Entry(work.id_, work.timeout_, work.next_run_ + std::chrono::milliseconds(work.timeout_), true, work.event_handler_, true));
+                    }
+                }
+                timer_lock.unlock();
 
-					work_queue_.insert(where.base(), work);
+                //插入队列
+                work_concurrentqueue_.enqueue_bulk(v_expired.begin(), v_expired.size());
 
-					work_queue_cond_.notify_one();*/
-					work_concurrentqueue_.enqueue(work);
-
-				}
-
-				timer_lock.lock();
-				if (work.repeat_)
-				{
-					work.next_run_ = std::chrono::steady_clock::now() + std::chrono::milliseconds(work.timeout_);
-
-					timers_set_.insert(std::move(work));
-				}
-
-			}
-		}
-	}
+            }
+        }
+    }
 
 }
--- MsgQ/dispatch_queue.h
+++ MsgQ/dispatch_queue.h
@@ -14,239 +14,234 @@
 class DispatchQueue
 {
 public:
-	using  EventFunc = std::function<void()>;
-	using time_point = std::chrono::steady_clock::time_point;
+    using  EventFunc = std::function<void()>;
+    using  time_point = std::chrono::steady_clock::time_point;
 
-	struct Event_Entry
-	{
-		uint64_t id_;
-		uint64_t timeout_;
-		time_point next_run_;
-		bool repeat_;
-		EventFunc event_handler_;
-		bool from_timer_;
-		Event_Entry() {}
-		Event_Entry(uint64_t id,
-			uint64_t timeout,
-			time_point next_run,
-			bool repeat,
-			EventFunc event_handler,
-			bool from_timer = true) :
-			id_(id),
-			timeout_(timeout),
-			next_run_(next_run),
-			repeat_(repeat),
-			event_handler_(event_handler),
-			from_timer_(from_timer)
-		{
-		}
+    struct Event_Entry
+    {
+        uint64_t id_;
+        uint64_t timeout_;
+        time_point next_run_;
+        bool repeat_;
+        EventFunc event_handler_;
+        bool from_timer_;
+        Event_Entry() {}
+        Event_Entry(uint64_t id,
+            uint64_t timeout,
+            time_point next_run,
+            bool repeat,
+            EventFunc event_handler,
+            bool from_timer = true) :
+            id_(id),
+            timeout_(timeout),
+            next_run_(next_run),
+            repeat_(repeat),
+            event_handler_(event_handler),
+            from_timer_(from_timer)
+        {
+        }
 
-		Event_Entry(const Event_Entry& o)
-		{
-			this->id_ = o.id_;
-			this->timeout_ = o.timeout_;
-			this->next_run_ = o.next_run_;
-			this->repeat_ = o.repeat_;
-			this->event_handler_ = o.event_handler_;
-			this->from_timer_ = o.from_timer_;
-		}
-		Event_Entry(Event_Entry&& o)
-		{
-			this->id_ = o.id_;
-			this->timeout_ = o.timeout_;
-			this->next_run_ = o.next_run_;
-			this->repeat_ = o.repeat_;
-			this->event_handler_ = std::move(o.event_handler_);
-			this->from_timer_ = o.from_timer_;
-		}
-		Event_Entry& operator=(const Event_Entry& o)
-		{
-			if (this != &o)
-			{
-				this->id_ = o.id_;
-				this->timeout_ = o.timeout_;
-				this->next_run_ = o.next_run_;
-				this->repeat_ = o.repeat_;
-				this->event_handler_ = o.event_handler_;
-				this->from_timer_ = o.from_timer_;
-			}
-			return *this;
-		}
-		Event_Entry& operator=(Event_Entry&& o)
-		{
-			if (this != &o)
-			{
-				this->id_ = o.id_;
-				this->timeout_ = o.timeout_;
-				this->next_run_ = o.next_run_;
-				this->repeat_ = o.repeat_;
-				this->event_handler_ = std::move(o.event_handler_);
-				this->from_timer_ = o.from_timer_;
-			}
+        Event_Entry(const Event_Entry& o)
+        {
+            this->id_ = o.id_;
+            this->timeout_ = o.timeout_;
+            this->next_run_ = o.next_run_;
+            this->repeat_ = o.repeat_;
+            this->event_handler_ = o.event_handler_;
+            this->from_timer_ = o.from_timer_;
+        }
+        Event_Entry(Event_Entry&& o)
+        {
+            this->id_ = o.id_;
+            this->timeout_ = o.timeout_;
+            this->next_run_ = o.next_run_;
+            this->repeat_ = o.repeat_;
+            this->event_handler_ = std::move(o.event_handler_);
+            this->from_timer_ = o.from_timer_;
+        }
+        Event_Entry& operator=(const Event_Entry& o)
+        {
+            if (this != &o)
+            {
+                this->id_ = o.id_;
+                this->timeout_ = o.timeout_;
+                this->next_run_ = o.next_run_;
+                this->repeat_ = o.repeat_;
+                this->event_handler_ = o.event_handler_;
+                this->from_timer_ = o.from_timer_;
+            }
+            return *this;
+        }
+        Event_Entry& operator=(Event_Entry&& o)
+        {
+            if (this != &o)
+            {
+                this->id_ = o.id_;
+                this->timeout_ = o.timeout_;
+                this->next_run_ = o.next_run_;
+                this->repeat_ = o.repeat_;
+                this->event_handler_ = std::move(o.event_handler_);
+                this->from_timer_ = o.from_timer_;
+            }
 
-			return *this;
-		}
+            return *this;
+        }
 
-	};
-	struct CompareLessNextRun
-	{
-		bool operator()(const Event_Entry& left, const Event_Entry& right) const
-		{
-			return left.next_run_ < right.next_run_;
-		}
-	};
+    };
+    struct CompareLessNextRun
+    {
+        bool operator()(const Event_Entry& left, const Event_Entry& right) const
+        {
+            return left.next_run_ < right.next_run_;
+        }
+    };
 
-	static DispatchQueue& GetDefaultDispatchQueue()
-	{
-		static DispatchQueue s_dispatchqueue;
-		return s_dispatchqueue;
-	}
+    static DispatchQueue& GetDefaultDispatchQueue()
+    {
+        static DispatchQueue s_dispatchqueue;
+        return s_dispatchqueue;
+    }
 public:
-	DispatchQueue();
-	~DispatchQueue();
+    DispatchQueue();
+    ~DispatchQueue();
 
-	DispatchQueue(const DispatchQueue&) = delete;
-	DispatchQueue(DispatchQueue&&) = delete;
-	DispatchQueue& operator=(const DispatchQueue&) = delete;
-	DispatchQueue& operator=(DispatchQueue&&) = delete;
+    DispatchQueue(const DispatchQueue&) = delete;
+    DispatchQueue(DispatchQueue&&) = delete;
+    DispatchQueue& operator=(const DispatchQueue&) = delete;
+    DispatchQueue& operator=(DispatchQueue&&) = delete;
 
-	template<typename Fn>
-	void DispatchAsync(Fn&& func)
-	{
-		work_concurrentqueue_.enqueue(Event_Entry(0, 0, time_point(), 0, std::forward<Fn>(func), false));
-	}
-	template<class F, class... Args>
-	void DispatchAsync(F&& f, Args&&... args)
-	{
-		auto func = std::bind(std::forward<F>(f), std::forward<Args>(args)...);
-		std::function<void()> task = func;
-		DispatchAsync(std::move(task));
-	}
+    template<typename Fn>
+    void DispatchAsync(Fn&& func)
+    {
+        work_concurrentqueue_.enqueue(Event_Entry(0, 0, time_point(), false, std::forward<Fn>(func), false));
+    }
+    template<class F, class... Args>
+    void DispatchAsync(F&& f, Args&&... args)
+    {
+        auto func = std::bind(std::forward<F>(f), std::forward<Args>(args)...);
+        std::function<void()> task = func;
+        DispatchAsync(std::move(task));
+    }
 
-	template<typename Fn>
-	void DispatchSync(Fn&& func)
-	{
-		std::mutex sync_mtx;
-		std::unique_lock< decltype(sync_mtx) > sync_lock(sync_mtx);
-		std::condition_variable sync_cond;
-		std::atomic< bool > completed(false);
+    template<typename Fn>
+    void DispatchSync(Fn&& func)
+    {
+        std::mutex sync_mtx;
+        std::unique_lock< decltype(sync_mtx) > sync_lock(sync_mtx);
+        std::condition_variable sync_cond;
+        std::atomic< bool > completed(false);
 
-		{
-			work_concurrentqueue_.enqueue(Event_Entry(0, 0, time_point(), 0, [&]()
-				{
-					std::forward<Fn>(func)();
+        {
+            work_concurrentqueue_.enqueue(Event_Entry(0, 0, time_point(), false, [&]()
+                {
+                    std::forward<Fn>(func)();
 
-					std::unique_lock<std::mutex> sync_cb_lock(sync_mtx);
-					completed = true;
-					sync_cond.notify_one();
+                    std::unique_lock<std::mutex> sync_cb_lock(sync_mtx);
+                    completed = true;
+                    sync_cond.notify_one();
 
-				}, false));
-		}
+                }, false));
+        }
 
-		sync_cond.wait(sync_lock, [&] { return completed.load(); });
-	}
+        sync_cond.wait(sync_lock, [&] { return completed.load(); });
+    }
 
-	template<class F, class... Args>
-	void DispatchSync(F&& f, Args&&... args)
-	{
-		std::function<void()> task = std::bind(std::forward<F>(f), std::forward<Args>(args)...);
+    template<class F, class... Args>
+    void DispatchSync(F&& f, Args&&... args)
+    {
+        std::function<void()> task = std::bind(std::forward<F>(f), std::forward<Args>(args)...);
 
-		DispatchSync(std::move(task));
-	}
-
-
-	template<class F, class... Args>
-	auto Dispatch(F&& f, Args&&... args)  -> std::future<typename std::result_of<F(Args...)>::type>
-	{
-		using return_type = typename std::result_of<F(Args...)>::type;
-
-		auto task = std::make_shared< std::packaged_task<return_type()> >(
-			std::bind(std::forward<F>(f), std::forward<Args>(args)...)
-			);
-
-		std::future<return_type> res = task->get_future();
-
-		DispatchAsync([task]() { (*task)(); });
-
-		return res;
-	}
+        DispatchSync(std::move(task));
+    }
 
 
-	uint64_t SetTimer(uint64_t milliseconds_timeout, EventFunc fun, bool repeat = true);
+    template<class F, class... Args>
+    auto Dispatch(F&& f, Args&&... args)  -> std::future<typename std::result_of<F(Args...)>::type>
+    {
+        using return_type = typename std::result_of<F(Args...)>::type;
 
-	template<class F, class... Args>
-	uint64_t SetTimer(uint64_t milliseconds_timeout, bool repeat, F&& f, Args&&... args)
-	{
-		auto func = std::bind(std::forward<F>(f), std::forward<Args>(args)...);
-		std::function<void()> task = func;
+        auto task = std::make_shared< std::packaged_task<return_type()> >(
+            std::bind(std::forward<F>(f), std::forward<Args>(args)...)
+            );
 
-		return SetTimer(milliseconds_timeout, task, repeat);
-	}
-	bool CancelTimer(uint64_t timer_id);
-	bool IsRunning()
-	{
-		return (timer_thread_started_ && work_queue_thread_started && !quit_);
-	}
-	void Join()
-	{
-		if (timer_thread_.joinable())
-			timer_thread_.join();
+        std::future<return_type> res = task->get_future();
 
-		if (work_queue_thread_.joinable())
-			work_queue_thread_.join();
-	}
-	void Stop()
-	{
-		quit_ = true;
-		work_queue_thread_started = false;
-		timer_thread_started_ = false;
-	}
+        DispatchAsync([task]() { (*task)(); });
+
+        return res;
+    }
+
+
+    uint64_t SetTimer(uint64_t milliseconds_timeout, EventFunc fun, bool repeat = true);
+
+    template<class F, class... Args>
+    uint64_t SetTimer(uint64_t milliseconds_timeout, bool repeat, F&& f, Args&&... args)
+    {
+        auto func = std::bind(std::forward<F>(f), std::forward<Args>(args)...);
+        std::function<void()> task = func;
+
+        return SetTimer(milliseconds_timeout, task, repeat);
+    }
+    bool CancelTimer(uint64_t timer_id);
+    bool IsRunning()
+    {
+        return (timer_thread_started_ && work_queue_thread_started && !quit_);
+    }
+    void Join()
+    {
+        if (timer_thread_.joinable())
+            timer_thread_.join();
+
+        if (work_queue_thread_.joinable())
+            work_queue_thread_.join();
+    }
+    void Stop()
+    {
+        quit_ = true;
+        work_queue_thread_started = false;
+        timer_thread_started_ = false;
+    }
 private:
-	void DispatchThreadProc();
-	void TimerThreadProc();
-	bool InitThread()
-	{
-		try
-		{
-			std::thread t(&DispatchQueue::DispatchThreadProc, this);
-			work_queue_thread_ = std::move(t);
+    void DispatchThreadProc();
+    void TimerThreadProc();
+    bool InitThread()
+    {
+        try
+        {
+            std::thread t(&DispatchQueue::DispatchThreadProc, this);
+            work_queue_thread_ = std::move(t);
 
-			std::thread t1(&DispatchQueue::TimerThreadProc, this);
-			timer_thread_ = std::move(t1);
+            std::thread t1(&DispatchQueue::TimerThreadProc, this);
+            timer_thread_ = std::move(t1);
 
-			//std::unique_lock< decltype(work_queue_mtx_) >  work_queue_lock(work_queue_mtx_);
-			std::unique_lock<decltype(timer_mtx_)> timer_lock(timer_mtx_);
-			//work_queue_cond_.wait(work_queue_lock, [this] { return work_queue_thread_started.load(); });
-			timer_cond_.wait(timer_lock, [this] { return timer_thread_started_.load(); });
-		}
-		catch (const std::exception&)
-		{
-			return false;
-		}
 
-		return true;
-	};
+            std::unique_lock<decltype(timer_mtx_)> timer_lock(timer_mtx_);
+            timer_cond_.wait(timer_lock, [this] { return timer_thread_started_.load(); });
+        }
+        catch (const std::exception&)
+        {
+            return false;
+        }
+
+        return true;
+    };
 private:
 
-	std::thread  work_queue_thread_;
-	std::thread  timer_thread_;
+    std::thread  work_queue_thread_;
+    std::thread  timer_thread_;
 
-	std::atomic< bool > work_queue_thread_started;
-	std::atomic< bool > timer_thread_started_;
-	std::atomic<bool> quit_;
+    std::atomic< bool > work_queue_thread_started;
+    std::atomic< bool > timer_thread_started_;
+    std::atomic<bool> quit_;
 
-	/*std::mutex work_queue_mtx_;
-	std::condition_variable work_queue_cond_;
-	std::deque< Event_Entry> work_queue_;*/
-	moodycamel::ConcurrentQueue<Event_Entry> work_concurrentqueue_;
+    moodycamel::ConcurrentQueue<Event_Entry> work_concurrentqueue_;
 
-	std::mutex timer_mtx_;
-	std::condition_variable timer_cond_;
-	std::multiset<Event_Entry, CompareLessNextRun> timers_set_;
+    std::mutex timer_mtx_;
+    std::condition_variable timer_cond_;
+    std::multiset<Event_Entry, CompareLessNextRun> timers_set_;
 
-	uint64_t generate_timer_id_;
-	std::atomic<bool> fall_through_;
-
+    uint64_t generate_timer_id_;
+    std::atomic<bool> fall_through_;
 };
 
 #endif
